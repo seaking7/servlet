3fafcb0060fdc786df8cf4eeabb137b4a2e1e862
taekyung <taekyung@lguplus.co.kr>
2022-03-09 11:47:54 +0900

    Initial commit Servlet Header 출력.

---
+++ src/test/java/tk/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
