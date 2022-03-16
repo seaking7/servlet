9df1a17996667eade252ebc467e360a43d07ea7d
Kim Taekyung <seaking7@gmail.com>
2022-03-16 21:02:39 +0900

    servlet V3에서 V4간 것처럼, Model파라미터로 처리, 리턴 String으로 지정

--- src/main/java/tk/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/tk/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -11,17 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
+@RequestMapping("/springmvc/v2/members")
 public class SpringMemberControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/springmvc/v2/members/new-form")
+    @RequestMapping("/new-form")
     public ModelAndView newForm() {
         System.out.println("SpringMemberFormControllerV1.process");
         return new ModelAndView("new-form");
     }
 
-    @RequestMapping("/springmvc/v2/members")
+    @RequestMapping
     public ModelAndView members() {
         List<Member> members = memberRepository.findAll();
         ModelAndView mv = new ModelAndView("members");
@@ -29,7 +30,7 @@ public class SpringMemberControllerV2 {
         return mv;
     }
 
-    @RequestMapping("/springmvc/v2/members/save")
+    @RequestMapping("/save")
     public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
---
+++ src/main/java/tk/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,40 @@
+package tk.servlet.web.springmvc.v3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tk.servlet.domain.member.Member;
+import tk.servlet.domain.member.MemberRepository;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public String newForm() {
+        System.out.println("SpringMemberFormControllerV1.process");
+        return "new-form";
+    }
+
+    @RequestMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members";
+    }
+
+    @RequestMapping("/save")
+    public String save(@RequestParam("username") String username,
+                             @RequestParam("age") int age,
+                             Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+    }
+}
